faf08afac526814dfed25c11068dd88894146da4
muyangkkong <137609813+muyangkkong@users.noreply.github.com>
2024-08-21 21:09:33 +0900

    8/21 예원 수정

---
+++ Awakein/Assets/Scripts/TornedDollManager copy.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TornedDollManagercopy : MonoBehaviour
+{
+     ImageChange imageChange;
+     public List<GameObject> Appearing=new List<GameObject>(); 
+     Canvas canvas;
+     GameObject arm;
+     public InvenManager invenManager;
+    // Start is called before the first frame update
+    bool ONce=true;
+    int n=0;
+    void Start()
+    {
+        imageChange=GetComponent<ImageChange>();
+        canvas=gameObject.transform.parent.GetComponent<Canvas>();
+    }
+
+    // Update is called once per frame
+    
+    public void Switch(){
+        if (!ONce) return;
+        if (ONce){
+            imageChange.SwitchSprite(gameObject, "doll");
+          arm=Instantiate(Appearing[0],canvas.transform);
+         Appearing[1].SetActive(true);
+          arm.transform.position=new Vector3(transform.position.x+90,transform.position.y+15,transform.position.z);
+           StartCoroutine(Arm_torning());
+           ONce=false;
+        }
+        }
+
+    
+    
+    public void ClosePopUp()
+    {
+        canvas.gameObject.SetActive(false);
+    }
+    
+    public void KeyClicked()
+    {
+        invenManager.ItemMap["Doll"].IsUsed = true;
+        invenManager.RemoveItem("Doll");
+        if( invenManager.ItemMap["Doll"].IsUsed)
+        Debug.Log("DollDisappeared");
+        invenManager.ItemAdder("RadioBattery");
+        
+    }
+    public IEnumerator Arm_torning(){
+        
+        Vector3 startposition=arm.transform.position;
+        Vector3 endposition=new Vector3(startposition.x+20,startposition.y-20,startposition.z);
+        float elapsedTime=0f;
+        float time=2f;
+        while(elapsedTime < time){
+            arm.transform.position=Vector3.Lerp(startposition,endposition,elapsedTime/time);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+        arm.transform.position=endposition;
+        Debug.Log("DOne");
+    }
+}
+    
+        
+        
